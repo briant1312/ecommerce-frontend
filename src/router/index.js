3125ff4ecfb8fe5a7b760b27f41ba3a462046fb6
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-08-18 13:29:06 -0400

    rename view compoents to acheive consistent naming

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import Category from '../views/Category.vue'
-import NotFound from '../views/NotFound.vue'
+import CategoryView from "../views/CategoryView.vue"
+import NotFoundView from "../views/NotFoundView.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,13 +14,13 @@ const router = createRouter({
     {
       path: '/:category',
       name: 'category',
-      component: Category,
+      component: CategoryView,
       props: true
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'notfound',
-      component: NotFound,
+      component: NotFoundView,
     },
     // {
     //   path: '/about',

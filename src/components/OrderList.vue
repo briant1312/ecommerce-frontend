aa924b8073c4cf5f1fbdebdbee7677da9479bc6e
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-08-29 12:39:46 -0400

    display order total on checkout page

--- src/components/OrderList.vue
+++ src/components/OrderList.vue
@@ -5,12 +5,18 @@
 
   const orderItems = ref([]);
   const { cartId } = inject("cart");
+  const orderTotal = ref(0);
 
   async function getData() {
     if (!cartId.value) return;
     
     const items = await(getItems(cartId.value));
     orderItems.value = items;
+    orderTotal.value = 0;
+    items.forEach(item => {
+      let price = item.price * item.qty;
+      orderTotal.value += price;
+    })
   }
   getData();
 
@@ -21,7 +27,17 @@
 
 <template>
   <OrderListItem @fetch-data="getData" v-for="item of orderItems" :item="item"/>
+  <p class="order-total">Order Total: ${{ orderTotal.toFixed(2) }}</p>
+  <button>Checkout</button>
 </template>
 
 <style scoped>
+.order-total {
+  margin-left: 10rem;
+  font-size: 1.3rem;
+}
+
+button {
+  margin-left: 10rem;
+}
 </style>
